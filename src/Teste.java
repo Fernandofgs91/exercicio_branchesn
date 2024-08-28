6051710edb043c458e59a8daedb73355506291fc
Fernandofgs91 <162770609+Fernandofgs91@users.noreply.github.com>
2024-08-28 11:32:46 -0300

    ok

--- src/Teste.java
+++ src/Teste.java
@@ -3,6 +3,24 @@ public class Teste {
     private int idade;
     git add src/Teste.java
     private String nome;
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public git getAdd() {
+        return add;
+    }
+    public void setAdd(git add) {
+        this.add = add;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
 
 }
